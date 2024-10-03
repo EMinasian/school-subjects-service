8bfad6b24d648163e958f73aa3816dfbf5958992
erik.minasian <eminasian@webmd.net>
2024-10-04 01:46:11 +0400

    feat: allow cross origin requests

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const getTeachers = require('./utils/getTeachers')
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 app.get('/health/_ping', (req, res) => {
     res.status(200).json({message: 'The service is working.'})
 })
