b98391c2b6d1fcae79045cce97b6dfcc37b66293
erik.minasian <eminasian@webmd.net>
2024-10-05 11:23:38 +0400

    fix: separate schema file for types and schema

--- server.js
+++ server.js
@@ -1,101 +1,12 @@
 const express = require('express')
 const cors = require("cors");
 const expressGraphQL = require("express-graphql").graphqlHTTP;
-const {
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLList,
-  GraphQLInt,
-  GraphQLNonNull,
-} = require("graphql");
-const { getAllSubjects, getSubjectStudents, getSubjectTeacher, getSubjectById } = require('./utils/subjects')
-const { getAllTeachers } = require('./utils/teachers')
-
-
-const StudentType = new GraphQLObjectType({
-  name: 'Student',
-  description: 'Represents a student',
-  fields: () => ({
-    firstname: { type: new GraphQLNonNull(GraphQLString) },
-    lastname: { type: new GraphQLNonNull(GraphQLString) }
-  })
-})
-
-const SubjectType = new GraphQLObjectType({
-  name: 'Subject',
-  description: 'Represents a subject',
-  fields: () => ({
-    id: { type: new GraphQLNonNull(GraphQLInt) },
-    title: { type: new GraphQLNonNull(GraphQLString) },
-    description: { type: new GraphQLNonNull(GraphQLString) },
-    class: { type: GraphQLString },
-    teacher: { 
-      type: TeacherType,
-      resolve: (subject) => {
-        const teacher = getSubjectTeacher(subject.id)
-        return teacher
-      } 
-    },
-    students: { 
-      type: new GraphQLList(StudentType),
-      resolve: (subject) => {
-        const students = getSubjectStudents(subject.id)
-        return students
-      }
-    }
-  })
-})
-
-const TeacherType = new GraphQLObjectType({
-  name: 'Teacher',
-  description: 'Represents a teacher',
-  fields: () => ({
-    id: { type: new GraphQLNonNull(GraphQLInt) },
-    firstname: { type: new GraphQLNonNull(GraphQLString) },
-    lastname: { type: new GraphQLNonNull(GraphQLString) },
-    email: { type: GraphQLString },
-    room: { type: GraphQLString }
-  })
-})
-
-const RootQueryType = new GraphQLObjectType({
-  name: 'Query',
-  description: "The root query",
-  fields: () => ({
-    subjects: {
-      type: new GraphQLList(SubjectType),
-      description: "The list of subjects",
-      args: { 
-        subjectId: { type: GraphQLInt } 
-      },
-      resolve: (parent, args) => {
-        const { subjectId } = args
-        if (subjectId) {
-          return getSubjectById(subjectId)
-        }
-        return getAllSubjects()
-      }
-    },
-    teachers: {
-      type: new GraphQLList(TeacherType),
-      description: "The list of teachers",
-      resolve: () => {
-        const teachers = getAllTeachers()
-        return teachers
-      }
-    }
-  })
-})
-
-const schema = new GraphQLSchema({
-  query: RootQueryType
-})
+const schema = require("./graphql/schema.js")
 
 const app = express()
 
+// to avoid cors issues for server to server requests
 app.use(cors());
-
 
 app.get('/health/_ping', (req, res) => {
     res.status(200).json({message: 'The service is working.'})
